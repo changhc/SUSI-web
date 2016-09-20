7d3c994533b6af8275ee08da836cbc2bcd915663
Charlie Chang (Adecco/Student) <v-ccharl@microsoft.com>
2016-09-20 14:18:29 +0800

    root page

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ server.listen(process.env.PORT || process.env.port || 3000, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-server.get('/', function(req, res, next){
-	res.send(200, "Hello World!");
-});
+server.get('/', restify.serveStatic({
+	directory: __dirname,
+	default: './index.html'
+}));
 
 server.get('/list.json', restify.serveStatic({
 	directory: __dirname,
